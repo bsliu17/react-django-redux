191466fba707d9b734a2bc55f97b76ef81fe6239
Bryan Liu <bsliu17@gmail.com>
2018-07-02 02:32:35 -0700

    initi

---
+++ backend/notes.md
@@ -0,0 +1,10 @@
+# Django Notes
+
+## Projects
+* project is the main skeleton, a project can have many apps
+* settings are set at project level
+* project contains URLs which are mapped to apps
+
+## Apps
+* models are set here
+* 
--- backend/todo_api/settings.py
+++ backend/todo_api/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party framework
+    'rest_framework',
+
     # Our Apps
     'todos',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
